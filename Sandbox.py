c443787bbceaf3397ecc066bc575526a6f1da18b
HubMcDeyo <mcdeyo08@gmail.com>
2025-09-26 15:39:18 +0000

    Minor changes (load time)

--- Sandbox.py
+++ Sandbox.py
@@ -3,8 +3,9 @@ def main() -> None: #Define the function main (codespace #1)
 Width = int(input("Width:")) #input width
 Length = int(input("Length:")) #input length
 Area = Width * Length #Area =length*width
-Area_mowed = Area / (40 * 60) #The area she mows per hour is the total area divided by 40 ft/min *60 min
-Cost = 30 * Area_mowed #Cost=Area mowed per hour *cost per hour
+Time = 10 + (Area / 40) #Time=10 minutes (load time) + (Area / 40) where 40 is the rate she mows in ft/min.
+Area_Time = Area / Time #The area she mows per hour is the total area divided by the Time.
+Cost = (30 / 60) * Area_Time #Cost=Area mowed per hour *cost per minute (30 / 60).
 print(f"${Cost:.2f}") #output cost to 2 decimals
 
 
